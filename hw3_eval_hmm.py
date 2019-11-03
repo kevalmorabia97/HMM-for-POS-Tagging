e8a216eab4604d10ae42a76fe76c9ac1bfe9df9c
kevalmorabia97 <kevalmorabia97@gmail.com>
2019-11-03 18:10:07 -0500

    hmm implementation done

--- hw3_eval_hmm.py
+++ hw3_eval_hmm.py
@@ -8,63 +8,76 @@
 ## Part 1:
 ## Evaluate the output of your bigram HMM POS tagger
 ##
-import os.path
+import numpy as np
 import sys
-from operator import itemgetter
 
-# A class for evaluating POS-tagged data
+from hw3_hmm import HMM, TaggedWord
+
+
 class Eval:
-    ################################
-    #intput:                       #
-    #    goldFile: string          #
-    #    testFile: string          #
-    #output: None                  #
-    ################################
+    """
+    A class for evaluating POS-tagged data
+    """
     def __init__(self, goldFile, testFile):
-        print("Your task is to implement an evaluation program for POS tagging")
+        hmm = HMM()
+        gold_sens = hmm.readLabeledData(goldFile)
+        test_sens = hmm.readLabeledData(testFile)
+        
+        self.tags = set()
+        for sen in gold_sens:
+            for tagged_word in sen:
+                self.tags.add(tagged_word.tag)
+        
+        self.tags = sorted(self.tags)
+        n_tags = len(self.tags)
+        self.tag_to_ind = {t:i for i,t in enumerate(self.tags)}
+        self.ind_to_tag = {i:t for t,i in self.tag_to_ind.items()}
 
-    ################################
-    #intput: None                  #
-    #output: float                 #
-    ################################
+        total_sens = len(gold_sens)
+        correct_sens = 0.0
+        self.confusion_matrix = np.zeros((n_tags, n_tags), dtype=np.int32)
+        for i in range(len(gold_sens)):
+            for j in range(len(gold_sens[i])):
+                self.confusion_matrix[self.tag_to_ind[gold_sens[i][j].tag], self.tag_to_ind[test_sens[i][j].tag]] += 1
+            
+            if [tw.tag for tw in gold_sens[i]] ==  [tw.tag for tw in test_sens[i]]:
+                correct_sens += 1
+        self.sentence_acc = correct_sens / total_sens
+
     def getTokenAccuracy(self):
-        print("Return the percentage of correctly-labeled tokens")
-        return 1.0
+        """
+        Return the percentage of correctly-labeled tokens
+        """
+        return self.confusion_matrix.diagonal().sum()/self.confusion_matrix.sum()
 
-    ################################
-    #intput: None                  #
-    #output: float                 #
-    ################################
     def getSentenceAccuracy(self):
-        print("Return the percentage of sentences where every word is correctly labeled")
-        return 1.0
+        """
+        Return the percentage of sentences where every word is correctly labeled
+        """
+        return self.sentence_acc
 
-    ################################
-    #intput:                       #
-    #    outFile: string           #
-    #output: None                  #
-    ################################
     def writeConfusionMatrix(self, outFile):
-        print("Write a confusion matrix to outFile; elements in the matrix can be frequencies (you don't need to normalize)")
+        """
+        Write a confusion matrix to outFile
+        """
+        with open(outFile, 'w') as f:
+            f.write(','.join(self.tags) + '\n')
+            for t in self.tags:
+                f.write(t + ',' + ','.join(self.confusion_matrix[self.tag_to_ind[t]].astype(str)) + '\n')
 
-    ################################
-    #intput:                       #
-    #    tagTi: string             #
-    #output: float                 #
-    ################################
     def getPrecision(self, tagTi):
-        print("Return the tagger's precision when predicting tag t_i")
-        return 1.0
+        """
+        Return the tagger's precision when predicting tag t_i
+        """
+        ind = self.tag_to_ind[tagTi]
+        return self.confusion_matrix[ind, ind] / self.confusion_matrix[:, ind].sum()
 
-    ################################
-    #intput:                       #
-    #    tagTi: string             #
-    #output: float                 #
-    ################################
-    # Return the tagger's recall on gold tag t_j
     def getRecall(self, tagTj):
-        print("Return the tagger's recall for correctly predicting gold tag t_j")
-        return 1.0
+        """
+        Return the tagger's recall for correctly predicting gold tag t_j
+        """
+        ind = self.tag_to_ind[tagTj]
+        return self.confusion_matrix[ind, ind] / self.confusion_matrix[ind].sum()
 
 
 if __name__ == "__main__":
--- hw3_hmm.py
+++ hw3_hmm.py
@@ -8,116 +8,203 @@
 ## Part 1:
 ## Train a bigram HMM for POS tagging
 ##
+from collections import defaultdict
+from math import log, exp
+import numpy as np
 import os.path
 import sys
-from operator import itemgetter
-from collections import defaultdict
-from math import log
 
 # Unknown word token
 UNK = 'UNK'
 
-# Class that stores a word and tag together
 class TaggedWord:
-    def __init__(self, taggedString):
-        parts = taggedString.split('_');
-        self.word = parts[0]
+    """
+    Class that stores a word and tag together
+    """
+    def __init__(self, taggedString, lowercase=False):
+        parts = taggedString.split('_')
+        self.word = parts[0] # convert all words to lower case
         self.tag = parts[1]
 
-# Class definition for a bigram HMM
+        if lowercase:
+            self.word = self.word.lower()
+
+
 class HMM:
-### Helper file I/O methods ###
-    ################################
-    #intput:                       #
-    #    inputFile: string         #
-    #output: list                  #
-    ################################
-    # Reads a labeled data inputFile, and returns a nested list of sentences, where each sentence is a list of TaggedWord objects
-    def readLabeledData(self, inputFile):
+    """
+    Class definition for a bigram HMM
+    """
+    def __init__(self, unknownWordThreshold=5):
+        self.minFreq = unknownWordThreshold # words occuring fewer than `unknownWordThreshold` times should be treated as UNK
+        
+        # compute following from train data
+        self.states = set()
+        self.n_tags = 0
+        self.vocab = set() 
+        self.tag_counts = defaultdict(float) 
+        self.bigram_tag_counts = defaultdict(float) 
+        self.word_tag_counts = defaultdict(float) 
+        self.init_log_tag_prob = defaultdict(float)
+
+    def readLabeledData(self, inputFile, lowercase=True):
+        """
+        Reads a labeled data inputFile, and returns a nested list of sentences, where each sentence is a list of TaggedWord objects
+        """
         if os.path.isfile(inputFile):
             file = open(inputFile, "r") # open the input file in read-only mode
-            sens = [];
+            sens = []
             for line in file:
                 raw = line.split()
-                sentence = []
+                sen = []
                 for token in raw:
-                    sentence.append(TaggedWord(token))
-                sens.append(sentence) # append this list as an element to the list of sentences
+                    sen.append(TaggedWord(token, lowercase))
+                sens.append(sen) # append this list as an element to the list of sentences
             return sens
         else:
             print("Error: unlabeled data file %s does not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
             sys.exit() # exit the script
 
-    ################################
-    #intput:                       #
-    #    inputFile: string         #
-    #output: list                  #
-    ################################
-    # Reads an unlabeled data inputFile, and returns a nested list of sentences, where each sentence is a list of strings
     def readUnlabeledData(self, inputFile):
+        """
+        Reads an unlabeled data inputFile, and returns a nested list of sentences, where each sentence is a list of strings
+        """
         if os.path.isfile(inputFile):
             file = open(inputFile, "r") # open the input file in read-only mode
-            sens = [];
+            sens = []
             for line in file:
-                sentence = line.split() # split the line into a list of words
-                sens.append(sentence) # append this list as an element to the list of sentences
+                sen = line.split() # split the line into a list of words
+                sens.append(sen) # append this list as an element to the list of sentences
             return sens
         else:
-            print("Error: unlabeled data file %s ddoes not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
+            print("Error: unlabeled data file %s does not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
             sys.exit() # exit the script
-### End file I/O methods ###
 
-    ################################
-    #intput:                       #
-    #    unknownWordThreshold: int #
-    #output: None                  #
-    ################################
-    # Constructor
-    def __init__(self, unknownWordThreshold=5):
-        # Unknown word threshold, default value is 5 (words occuring fewer than 5 times should be treated as UNK)
-        self.minFreq = unknownWordThreshold
-        ### Initialize the rest of your data structures here ###
+    def compute_vocab_and_states(self, train_data):
+        word_freq = defaultdict(int)
+        self.vocab.add(UNK)
 
-    ################################
-    #intput:                       #
-    #    trainFile: string         #
-    #output: None                  #
-    ################################
-    # Given labeled corpus in trainFile, build the HMM distributions from the observed counts
+        for sen in train_data:
+            for tagged_word in sen:
+                word_freq[tagged_word.word] = word_freq[tagged_word.word] + 1
+        
+        for sen in train_data:
+            for tagged_word in sen:
+                self.states.add(tagged_word.tag)
+                if word_freq[tagged_word.word] > self.minFreq:
+                    self.vocab.add(tagged_word.word)
+        
+        self.n_tags = len(self.states)
+
     def train(self, trainFile):
-        data = self.readLabeledData(trainFile) # data is a nested list of TaggedWords
-        #print("Your first task is to train a bigram HMM tagger from an input file of POS-tagged text")
+        """
+        Given labeled corpus in trainFile, build the HMM distributions from the observed counts
+        """
+        train_data = self.readLabeledData(trainFile) # train_data is a nested list of TaggedWords
+        self.compute_vocab_and_states(train_data)
+        
+        for sen in train_data:
+            for tagged_word in sen:
+                if not tagged_word.word in self.vocab:
+                    tagged_word.word = UNK
+            
+            self.tag_counts[sen[0].tag] += 1.0
+            self.word_tag_counts[(sen[0].word, sen[0].tag)] += 1.0
+            for i in range(1, len(sen)):
+                self.tag_counts[sen[i].tag] += 1.0
+                self.word_tag_counts[(sen[i].word, sen[i].tag)] += 1.0
+                self.bigram_tag_counts[(sen[i-1].tag, sen[i].tag)] += 1.0
+        
+        normalizer = 0
+        for tag in self.states:
+            self.init_log_tag_prob[tag] = log(self.tag_counts[tag])
+            normalizer += self.tag_counts[tag]
+        normalizer = log(normalizer)
+        for tag in self.states:
+            self.init_log_tag_prob[tag] -= normalizer
 
-    ################################
-    #intput:                       #
-    #     testFile: string         #
-    #    outFile: string           #
-    #output: None                  #
-    ################################
-    # Given an unlabeled corpus in testFile, output the Viterbi tag sequences as a labeled corpus in outFile
+    def get_log_transition_prob(self, tag, prev_tag):
+        """
+        return log[smoothed p(tag | prev_tag)]
+        """
+        return log ( (self.bigram_tag_counts[(prev_tag, tag)] + 1) / (self.tag_counts[tag] + self.n_tags) )
+
+    def get_log_emission_prob(self, word, tag):
+        """
+        return log[p(word | tag)]
+        """
+        if self.word_tag_counts[(word, tag)] == 0.0:
+            return float('-inf')
+        return log( (self.word_tag_counts[(word, tag)]) / (self.tag_counts[tag]) )
+
     def test(self, testFile, outFile):
+        """
+        Given an unlabeled corpus in testFile, output the Viterbi tag sequences as a labeled corpus in outFile
+        """
         data = self.readUnlabeledData(testFile)
+
         f=open(outFile, 'w+')
         for sen in data:
-            vitTags = self.viterbi(sen)
+            processed_sen = []
+            for word in sen:
+                if word.lower() not in self.vocab:
+                    processed_sen.append(UNK)
+                else:
+                    processed_sen.append(word.lower())
+
+            vitTags = self.viterbi(processed_sen)
             senString = ''
             for i in range(len(sen)):
                 senString += sen[i]+"_"+vitTags[i]+" "
             print(senString.rstrip(), end="\n", file=f)
 
-    ################################
-    #intput:                       #
-    #    words: list               #
-    #output: list                  #
-    ################################
-    # Given a list of words, runs the Viterbi algorithm and returns a list containing the sequence of tags
-    # that generates the word sequence with highest probability, according to this HMM
     def viterbi(self, words):
-        #print("Your second task is to implement the Viterbi algorithm for the HMM tagger")
-        # returns the list of Viterbi POS tags (strings)
-        return ["NULL"]*len(words) # this returns a dummy list of "NULL", equal in length to words
+        """
+        Given a list of words, runs the Viterbi algorithm and returns a list containing the sequence of tags
+        that generates the word sequence with highest probability, according to this HMM.
+        Returns: the list of Viterbi POS tags (strings)
+        """
+        tags = sorted(list(self.states))
+        n_words = len(words)
+        trellis = np.zeros((n_words, self.n_tags), dtype=np.float64)
+        trellis.fill(np.inf)
+        backpointers = np.zeros((n_words, self.n_tags), dtype=np.int32)
+
+        for j in range(self.n_tags):
+            trellis[0, j] = self.init_log_tag_prob[tags[j]] + self.get_log_emission_prob(words[0], tags[j])
+        
+        for i in range(1, n_words):
+            for j in range(self.n_tags):
+                for t in range(self.n_tags):
+                    temp = trellis[i-1, t] + self.get_log_transition_prob(tags[j], tags[t])
+                    if temp > trellis[i, j] or trellis[i, j] == np.inf:
+                        trellis[i, j] = temp
+                        backpointers[i, j] = t
+                trellis[i, j] += self.get_log_emission_prob(words[i], tags[j])
+        
+        best_end_tag = -1
+        best_log_prob = float('-inf')
+        for j in range(self.n_tags):
+            if trellis[-1, j] > best_log_prob:
+                best_end_tag = j
+                best_log_prob = trellis[-1, j]
+        
+        tag_seq = []
+        i = n_words - 1
+        while i >= 0:
+            tag_seq.append(tags[best_end_tag])
+            best_end_tag = backpointers[i, best_end_tag]
+            i -= 1
+
+        tag_seq.reverse()
+        return tag_seq
+
 
 if __name__ == "__main__":
+    print('Training model...')
     tagger = HMM()
     tagger.train('train.txt')
+
+    print('Testing...')
     tagger.test('test.txt', 'out.txt')
+
+    print('Done!')
